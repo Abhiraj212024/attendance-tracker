78789ab8f6a1f1809e75b29f2d81f2fe86c17690
Abhiraj212024 <abhirajraje1@gmail.com>
2026-01-03 13:37:12 +0530

    fixed exporting error

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,3 +3,5 @@ const router = express.Router()
 const registerController = require('../controllers/registerController')
 
 router.post('/', registerController.handleRegistration)
+
+module.exports = router
