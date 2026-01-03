396c3d7493eae7d033518c4a2c0c2445ff7f6b7e
Abhiraj212024 <abhirajraje1@gmail.com>
2026-01-03 18:22:01 +0530

    added registration page

---
+++ client/attendance-client/src/pages/Register.jsx
@@ -0,0 +1,82 @@
+import api from '../services/api'
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import styles from '../styles/auth.module.css'
+export default function Register(){
+    const navigate = useNavigate()
+    
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError("")
+        setLoading(true)
+
+        try {
+            await api.post('/register', {
+                name,
+                email,
+                password
+            })
+            console.log("Registration successful")
+            navigate('/login')
+
+        } catch (error) {
+            if (!err.response) {
+                setError("No server response")
+            } else if (err.response.status === 409) {
+                setError("Email already exists")
+            } else {
+                setError("Registration failed")
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <form className={styles.card} onSubmit={handleSubmit}>
+                <h2>Register</h2>
+                {error && <p className={styles.error}>{error}</p>}
+
+            <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            />
+
+            <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            />
+
+            <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            />
+
+            <button disabled={loading}>
+            {loading ? "Registering..." : "Register"}
+            </button>
+            </form>
+            <p className={styles.switch}>
+                Already have an account?{" "}
+                <Link to="/login">Login</Link>
+            </p>
+
+        </div>
+    )
+}
