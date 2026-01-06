3c67d3254e7d99d6937be8d9d2fe6ba789561e16
Abhiraj212024 <abhirajraje1@gmail.com>
2026-01-06 21:29:07 +0530

    added jwt token verification

---
+++ server/middleware/verifyJWT.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization
+
+    if(!authHeader?.startsWith('Bearer ')){
+        return res.status(401).send({
+            'message': 'token not received correctly'
+        })
+    }
+
+    const token = authHeader.split(' ')[1] //standard documentation procedure
+
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            if(err){
+                console.log(err)
+                return res.sendStatus(403) //invalid token
+            }
+            req.user = decoded
+            next()
+        }
+    )
+}
+
+module.exports = { verifyJWT }
